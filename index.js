34055b26877f4441b1251b9295b949d2ae3d1823
Andrey Zhygarev <andrii.zhygarev@dstarlab.com>
2021-06-15 17:32:14 +0300

    created first test route

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("api is running!!!");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server is running on port ${PORT}`);
+});
