7f4b45bae9d1571b97b16ac424cca28820945185
Andrey Zhygarev <andrii.zhygarev@dstarlab.com>
2021-06-16 14:29:35 +0300

    added comments to index.js

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ const path = require("path");
 const routes = require("./api/routes/index");
 const connectDB = require("./config/db");
 
+// Create app
 const app = express();
+// Connect to database
 connectDB();
 
-// middleware for req.body
+// Middleware for req.body
 app.use(express.json({ extended: false }));
 
+// Routes
 app.get("/", (req, res) => {
   res.send("api is running!!!");
 });
 routes(app);
 
+// Server port number
 const PORT = process.env.PORT || 5000;
 
+// Start
 app.listen(PORT, (req, res) => {
   console.log(`Server is running on port ${PORT}`);
 });
