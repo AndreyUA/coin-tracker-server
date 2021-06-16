e5634846a25095597241c0dd34191f88fcdd459d
Andrey Zhygarev <andrii.zhygarev@dstarlab.com>
2021-06-16 10:13:29 +0300

    connected mongo db

--- config/db.js
+++ config/db.js
@@ -1,4 +1,21 @@
 const mongoose = require("mongoose");
 const config = require("config");
 
-// So here I'll implement connection to MongoDB
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("mongo db connected!");
+  } catch (error) {
+    console.log(error.message);
+
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
 
+const connectDB = require("./config/db");
+
 const app = express();
+connectDB();
 
 app.use(express.json({ extended: false }));
 
