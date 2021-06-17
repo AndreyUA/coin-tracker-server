000ba216bd87067198a5cc891d17baf071d1e98c
Andrey Zhygarev <andrii.zhygarev@dstarlab.com>
2021-06-17 09:41:29 +0300

    implemented account login

--- api/routes/loginRoutes.js
+++ api/routes/loginRoutes.js
@@ -25,4 +25,62 @@ router.get("/", authMiddleware, async (req, res) => {
   }
 });
 
+// @route       POST api/login
+// @desc        Account login
+// @access      Public
+router.post(
+  "/",
+  [
+    check("email", "Correct email is required").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let family = await Family.findOne({ email });
+
+      if (!family) {
+        return res.status(400).json({
+          errors: [{ msg: "Invalid credentials" }],
+        });
+      }
+
+      const isPasswordMatch = await brcypt.compare(password, family.password);
+
+      if (!isPasswordMatch) {
+        return res.status(400).json({
+          errors: [{ msg: "Invalid credentials" }],
+        });
+      }
+
+      const payload = {
+        family: {
+          id: family.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 604800000 },
+        (err, token) => {
+          if (err) throw err;
+
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
 module.exports = router;
